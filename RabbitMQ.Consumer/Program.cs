83b2b4dffe149410486bc5a8bc4b1f767ee3d536
Padmini <Padmini@LAPTOP-7MKJPBPJ>
2026-06-18 22:27:32 +0530

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+// connection facotry 
+var factory = new ConnectionFactory { HostName = "localhost" };
+// open an new connection to my RabbitMQ broker
+using var connection = await factory.CreateConnectionAsync();
+//instantiate a channel - way to communicate with my broker instance
+using var channel = await connection.CreateChannelAsync();
+
+
+await channel.QueueDeclareAsync(
+    queue: "messages",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+Console.WriteLine("waitng for messages....");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (sender, eventArgs) =>
+{
+    byte[] body = eventArgs.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Received:{message}");
+
+    // ack to broker tht msg is received so msg can be removed from the queue 
+    // so any concurrent consumers wont be able to process this message
+    await ((AsyncEventingBasicConsumer)sender).Channel.BasicAckAsync(eventArgs.DeliveryTag,
+        multiple:false);
+};
+await channel.BasicConsumeAsync("messages", autoAck: false, consumer);
+
+Console.ReadLine();
---
+++ RabbitMQ.Producer/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+// connection facotry 
+var factory = new ConnectionFactory { HostName ="localhost"};
+// open an new connection to my RabbitMQ broker
+using var connection = await factory.CreateConnectionAsync();
+//instantiate a channel - way to communicate with my broker instance
+using var channel = await connection.CreateChannelAsync();
+#region
+// channel is how we can interact with a broker 
+// bind queue  , declare a queue , delete it , unbind etc
+// work with exchanges using exchange API's
+// there is also support for Acknowledging and consuming messages
+#endregion
+await channel.QueueDeclareAsync(
+    queue: "messages",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments:null);
+// producer to publish some message
+for (int i = 0; i < 10; i++)
+{
+    var message = $"{DateTime.UtcNow}-{Guid.NewGuid()}";
+    var body = Encoding.UTF8.GetBytes(message);
+    // publish to the channel instace queue creatd above
+    await channel.BasicPublishAsync(
+        exchange:string.Empty,
+        routingKey:"messages",
+        mandatory:true,
+        basicProperties: new BasicProperties {Persistent=true },
+        body:body);
+    Console.WriteLine($"Sent_{i}:{message}");
+    //delay to observe
+    await Task.Delay(5000);
+}
+
